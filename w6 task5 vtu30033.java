a1034097962eced00f69eca58409ca6622038829
vtu30033-create <vtu30033@veltech.edu.in>
2026-08-27 22:02:03 +0530

    Create w6 task5 vtu30033.java

---
+++ w6 task5 vtu30033.java
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int marsExploration(string s) {
+    int count = 0;
+
+    string expected = "SOS";
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != expected[i % 3]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = marsExploration(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
