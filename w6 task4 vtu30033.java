94601f2d5d952b752364436d38f66ccb4c906013
vtu30033-create <vtu30033@veltech.edu.in>
2026-08-27 21:59:25 +0530

    Implement rotateString method to check rotations

---
+++ w6 task4 vtu30033.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        // Lengths must be equal
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        // If goal is a rotation of s,
+        // it must appear inside s + s
+        String doubled = s + s;
+
+        return doubled.contains(goal);
+    }
+}
